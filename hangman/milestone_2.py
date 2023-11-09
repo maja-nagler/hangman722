148a6be84644d0220dc6b40b98c0e583733db0e3
maja-nagler <2760739N@student.gla.ac.uk>
2023-11-09 14:40:43 +0000

    Adds file milestone_2.py with a word list

---
+++ hangman/milestone_2.py
@@ -0,0 +1,7 @@
+#Step 1 & Step 2: creating a list and assigning it to a variable
+
+word_list = ["banana", "cherry", "blueberry", "mango", "apple"]
+
+#Step 3: print out the newly created list to the standard output (screen).
+
+print(word_list)
